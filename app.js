fa5e83762da11062c99709bd77e020880b6d57f0
Katan Patel <katanpatel86@gmail.com>
2019-02-27 17:17:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const missed = 0; //if the player guesses wrong 5 times, they lose the game
 const overlay = document.getElementById('overlay');
 const resetBtn = document.getElementById('btn__reset');
 
+const phrases = ['under the weather', 'heads up', 'jumping the gun', 'rain on your parade', 'close but no cigar'];
+
+//Attach a event listener to the “Start Game” button to hide the start screen overlay.
 document.addEventListener('click', function() {
   overlay.style.display = 'none';
 });
+
+function getRandomAsPhraseArray(arr){
+  //do stuff to any arr that is passed in
+    let randomPhrase = phrases[Math.floor(Math.random() * 5)];
+    return randomPhrase;
+}
