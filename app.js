513601958e8d20b4a6799a353bd8f1a305e088c1
Katan Patel <katanpatel86@gmail.com>
2019-02-28 17:47:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const missed = 0; //if the player guesses wrong 5 times, they lose the game
+let missed = 0; //if the player guesses wrong 5 times, they lose the game
 const overlay = document.getElementById('overlay');
 const resetBtn = document.getElementById('btn__reset');
-
 const phrases = ['under the weather', 'heads up', 'jumping the gun', 'rain on your parade', 'close but no cigar'];
 const phraseArray = getRandomPhraseAsArray(phrases);
+const letters = document.getElementsByClassName('letter');
+const heart = document.getElementsByTagName('img');
 //Attach a event listener to the “Start Game” button to hide the start screen overlay.
 document.addEventListener('click', function() {
   overlay.style.display = 'none';
@@ -16,16 +17,17 @@ function getRandomPhraseAsArray(arr){
     let randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
     return randomPhrase;
 }
+
 getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay(arr){
     // do stuff any arr that is passed in, and add to `#phrase ul`
-    for(let i = 0; i < arr.length; i += 1) {
-      let ul = document.getElementsByTagName('ul')[0];
-      let li = document.createElement('li');
-      li.textContent = arr[i];
-      ul.appendChild(li);
-      if (arr[i] != ' ') {
+    for(let i = 0; i < arr.length; i += 1) { //loop through the given array.
+      let ul = document.getElementsByTagName('ul')[0]; //locates the ul from html
+      let li = document.createElement('li'); //creates a new li
+      li.textContent = arr[i]; //places the random arr character to the text content
+      ul.appendChild(li); //creates a new li under the ul
+      if (arr[i] != ' ') { //checks to see if the li has a letter or space and reaches out to the CSS Class.
             li.className = "letter";
         } else {
             li.className = "space";
@@ -34,3 +36,28 @@ function addPhraseToDisplay(arr){
 }
 
 addPhraseToDisplay(phraseArray);
+
+function checkLetter(btn){
+  let correct = false;
+  for(let i = 0; i < letters.length; i += 1) {
+    if(btn.target.textContent === letters[i].textContent){
+      letters[i].classList.add("show");
+      correct = true;
+    }
+  }
+  return correct;
+  }
+
+qwerty.addEventListener('click', (e) => {
+    const letterFound = checkLetter(e);
+  if (e.target.tagName === 'BUTTON') {
+      e.target.className = "chosen";
+      e.target.disabled = 'true';
+      if (letterFound === false && missed < 5) {
+ 			heart[missed].setAttribute('src', 'images/lostHeart.png');
+ 			missed++;
+
+ 		}
+ 	}
+
+ });
